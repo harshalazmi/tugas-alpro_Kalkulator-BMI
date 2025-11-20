86c9dc0285b0cd369d5cd8a95ad59aa3fdb708d6
harshalazmi <harshalazmi@gmail.com>
2025-11-20 16:01:00 +0700

    Menambahkan file Java

---
+++ KalkulatorBMI.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class KalkulatorBMI {
+	public static void main(String[] args) {
+		//Memunculkan Kotak pesan
+		String inputBeratBadan = JOptionPane.showInputDialog("Masukan Berat Badan (kg):");
+		String inputTinggiBadan = JOptionPane.showInputDialog("Masukan Tinggi Badan (cm):");
+		
+		//Mengubah nilai string ke double (desimal)
+		double BeratB = Double.parseDouble(inputBeratBadan);
+		double TinggiB_cm = Double.parseDouble(inputTinggiBadan);
+		
+		// Konversi Niai cm ke meter
+		double TinggiB_m =TinggiB_cm /100.0;
+		
+		//Rumus BMI
+		double nilaibmi = BeratB / (TinggiB_m * TinggiB_m);
+		
+		//Membuat kategori
+		String Kategori = "";
+		if (nilaibmi < 18.5){
+			Kategori = "Kurus";
+		} else if (nilaibmi <= 24.9) {
+			Kategori = "Normal";
+		} else if (nilaibmi <= 29.9) {
+			Kategori = "Gemuk";
+		} else if (nilaibmi > 30.0) {
+			Kategori = "Obesitas";
+		}
+		
+		//Hasil kalkulasi
+		String hasil = "Nilai BMI : " + nilaibmi +
+				"\nKategori : " + Kategori;
+		JOptionPane.showMessageDialog(null, hasil, "Hasil BMI", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
